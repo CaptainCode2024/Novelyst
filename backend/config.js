45c52ea7b308336112982d034214dc1bfc002c75
Kavya <135150115+CaptainCode2024@users.noreply.github.com>
2025-09-10 11:25:50 +0530

    Fix: Comments

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-// MongoDB Cluster Conncection String
+// MongoDB Cluster Connection String
 export const NovelystClusterMongoURL = "mongodb+srv://CaptainCode2024:dbQ694WKtS61fX04@novelystcluster.pb65ulc.mongodb.net/?retryWrites=true&w=majority&appName=NovelystCluster"
